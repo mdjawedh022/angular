71e54cb6471ec2e209f06491a259a7603ddbb6f8
Md Jawed <mdjawedh22@gmail.com>
2023-10-19 14:00:29 +0530

    day-vi assignment fetching api show data in dom

--- day-vi/src/app/display-item/display-item.component.ts
+++ day-vi/src/app/display-item/display-item.component.ts
@@ -4,17 +4,16 @@ import { ExternalApiServiceService } from '../external-api-service.service';
 @Component({
   selector: 'app-display-item',
   templateUrl: './display-item.component.html',
-  styleUrls: ['./display-item.component.css']
+  styleUrls: ['./display-item.component.css'],
 })
 export class DisplayItemComponent {
-dataAppend:any;
-  constructor(private externalApiService:ExternalApiServiceService){}
+  dataAppend: any;
+  constructor(private externalApiService: ExternalApiServiceService) {}
 
-ngOnInit(){
-  this.externalApiService.dataFectch().subscribe((data:any)=>{
-this.dataAppend=data;
-console.log(data);
-
-  })
-}
+  ngOnInit() {
+    this.externalApiService.dataFectch().subscribe((data: any) => {
+      this.dataAppend = data;
+      console.log(data);
+    });
+  }
 }
